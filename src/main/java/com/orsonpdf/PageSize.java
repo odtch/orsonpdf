ea73ddb3456975bce04c2320f9249783e0cc9bfb
Reto Tschofenig, ODT Informatik GmbH <rt@odt.ch>
2018-11-16 18:35:46 +0100

    PageSize.A4

---
+++ src/main/java/com/orsonpdf/PageSize.java
@@ -0,0 +1,10 @@
+package com.orsonpdf;
+
+import java.awt.Rectangle;
+
+public class PageSize {
+	
+	public static final Rectangle A4 = new Rectangle( 595, 841 );
+	public static final Rectangle A4_landscape = new Rectangle( 841, 595 );
+	
+}
